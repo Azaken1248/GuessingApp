dec45df990f95a5e8a35ad5a055574f976bd7773
= <=>
2026-02-23 17:00:12 +0530

    Add Controller to main app

--- GuessingApp.java
+++ GuessingApp.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 /**
  * MAIN CLASS
  * 
- * Use Case 5: Game Result Storage
+ * Use Case 6: Game Restart & Exit
  * 
- * This class coordinates the complete game flow
- * and persists the final result after completion
+ * This class coordinates the complete game lifecycle,
+ * allowinf the player to replay or exit gracefully.
  * 
  * Responsibilites:
- * - Initialize game configuration 
- * - Accept and validate user guesses
- * - Generate hints when applicable
- * - Store game result at the end
+ * - Start a new game session 
+ * - Execute the guessing flow
+ * - Persist game results
+ * - Restart or exit based on user choice
  * 
  * 
  * @author Developer
- * @version 5.0
+ * @version 6.0
  */
 
 public class GuessingApp {
@@ -27,62 +27,70 @@ public class GuessingApp {
         System.out.println("===========================");
         System.out.println("Welcome to the Guessing App");
         System.out.println("===========================\n");
+        boolean restart;
 
-        /**
-         * Player name is captured once
-         * and stored along with game results
-         */
-        System.out.println("Enter Player Name: ");
-        String player = scanner.nextLine();
-        
-        GameConfig config = new GameConfig();
-        config.showRules();
-
-        
-        int attempts = 0;
-        int hintCount = 0;
-
-        /**
-         * Tracks weather the player
-         * successfully guessed the number.
-         */
-        boolean win = false;
-
-        /**
-         * Game loop runs until the player
-         * exhausts the maximum attempts.
-         */
-        while(attempts < config.getMaxAttempts()){
-            System.out.println("Enter your guess: ");
-            
+        do{
             /**
-             * User input is validated before
-             * being used on the game
-             */	
-            int guess = ValidationService.validateInput(scanner.nextLine());
-            attempts++;
-
-            String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
-            
-            if(!"CORRECT".equals(result) && hintCount < config.getMaxHints()){
-                hintCount++;
-                System.out.println(HintService.generateHint(config.getTargetNumber(), hintCount));    
-            }
-            
-            System.out.println(result);
-
-            /**
-             * Stop the loop immediately
-             * if correct number is guessed.
+             * Player name is captured once
+             * and stored along with game results
              */
-            if("CORRECT".equals(result)){
-                win = true;
-                break;
+            System.out.println("Enter Player Name: ");
+            String player = scanner.nextLine();
+            
+            GameConfig config = new GameConfig();
+            config.showRules();
+
+            
+            int attempts = 0;
+            int hintCount = 0;
+
+            /**
+             * Tracks weather the player
+             * successfully guessed the number.
+             */
+            boolean win = false;
+
+            /**
+             * Game loop runs until the player
+             * exhausts the maximum attempts.
+             */
+            while(attempts < config.getMaxAttempts()){
+                System.out.println("Enter your guess: ");
+                
+                /**
+                 * User input is validated before
+                 * being used on the game
+                 */	
+                int guess = ValidationService.validateInput(scanner.nextLine());
+                attempts++;
+
+                String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
+                
+                if(!"CORRECT".equals(result) && hintCount < config.getMaxHints()){
+                    hintCount++;
+                    System.out.println(HintService.generateHint(config.getTargetNumber(), hintCount));    
+                }
+                
+                System.out.println(result);
+
+                /**
+                 * Stop the loop immediately
+                 * if correct number is guessed.
+                 */
+                if("CORRECT".equals(result)){
+                    win = true;
+                    break;
+                }
             }
-        }
 
-        StorageService.saveResult(player, attempts, win);
+            StorageService.saveResult(player, attempts, win);
 
-        scanner.close();
+            /**
+             * Player decides weather to 
+             * restart the game or exit.
+             */
+            restart = GameController.restartGame(scanner);
+
+        }while(restart);
     }
 }
