d9a8b3ea26bbfc62040675788573377cb5a8be30
= <=>
2026-02-23 15:42:59 +0530

    Update Guessig app to use the validation service

--- GuessingApp.java
+++ GuessingApp.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 /**
  * MAIN CLASS
  * 
- * Coordinates the game flow
- * 1. Initialize game
- * 2. Accept user guesses
- * 3. Validate guesses
- * 4. Provide hints on wrong answer
- * 4. Stop when game ends
+ * Use Case 4: Error Handling & validation
+ * 
+ * This class coordinates the game execution while ensuring
+ * all user inputs are safely validated before processing.
+ * 
+ * Responsibilites:
+ * 1. Initialize game configuration 
+ * 2. Accept user input
+ * 3. Validate input using validationService
+ * 4. Handle game flow without crashing on invalid input
  * 
  * 
  * @author Developer
- * @version 3.0
+ * @version 4.0
  */
 
 public class GuessingApp {
 
-    static int hintCount = 1;
-    public static void main(String[] args) {
+    static int hintCount = 0;
+    public static void main(String[] args) throws InvalidInputException{
         System.out.println("Welcome to the Guessing App");
         
         GameConfig config = new GameConfig();
@@ -33,11 +37,21 @@ public class GuessingApp {
          */
         while(attempts < config.getMaxAttempts()){
             System.out.println("Enter your guess: ");
-            int guess = scanner.nextInt();
+            
+            /**
+             * User input is validated before
+             * being used on the game
+             */	
+            int guess = ValidationService.validateInput(scanner.nextLine());
             attempts++;
 
             String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
-            System.out.println(HintService.generateHint(config.getTargetNumber(), hintCount++));
+            
+            if(!"CORRECT".equals(result) && hintCount < config.getMaxHints()){
+                hintCount++;
+                System.out.println(HintService.generateHint(config.getTargetNumber(), hintCount));    
+            }
+            
             System.out.println(result);
 
             /**
